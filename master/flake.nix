16b15106e74109d9a28cf8d7b160d25b29bac159
hugosenari <hugosenari@gmail.com>
2022-03-12 15:23:59 -0300

    chore: re index sqlite3_abi

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A wrapper for SQLite'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-sqlite3_abi-master.flake = false;
+  inputs.src-sqlite3_abi-master.owner = "arnetheduck";
+  inputs.src-sqlite3_abi-master.ref   = "refs/heads/master";
+  inputs.src-sqlite3_abi-master.repo  = "nim-sqlite3-abi";
+  inputs.src-sqlite3_abi-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-sqlite3_abi-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-sqlite3_abi-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
